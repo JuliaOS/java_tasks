eb3edeee5cae5b98801ff95f93968cd8b32cc72f
JuliaOS <julia2790@mail.ru>
2017-04-15 22:39:31 +0300

    added: addressbook test for adding new contact

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String mobilePhone;
+    private final String workPhone;
+    private final String email1;
+    private final String email2;
+    private final String email3;
+
+    public ContactData(String firstName, String lastName, String address, String mobilePhone, String workPhone, String email1, String email2, String email3) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.mobilePhone = mobilePhone;
+        this.workPhone = workPhone;
+        this.email1 = email1;
+        this.email2 = email2;
+        this.email3 = email3;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public String getEmail3() {
+        return email3;
+    }
+}
