6d66e7f7f7e30b8a6aa0c3454a3af41aca115c72
JuliaOS <julia2790@mail.ru>
2017-04-24 21:22:57 +0300

    Added: Chrome and IE support

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/TestBase.java
@@ -3,13 +3,14 @@ package ru.stqa.pft.addressbook.test;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 
 /**
  * Created by Julia on 4/16/2017.
  */
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
     @BeforeMethod
     public void setUp() throws Exception {
