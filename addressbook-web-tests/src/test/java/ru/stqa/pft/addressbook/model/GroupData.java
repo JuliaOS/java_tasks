03f912a0a0fdefd097d08c6bbc07927db2d8bb35
JuliaOS <julia2790@mail.ru>
2017-05-24 21:55:27 +0300

    Added: address_in_groups db table support

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,10 +4,9 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @XStreamAlias("group")
 @Entity
@@ -25,12 +24,18 @@ public class GroupData {
     @Column(name = "group_footer")
     @Type(type = "text")
     private String footer;
+    @ManyToMany(mappedBy = "groups")
+    private Set<ContactData> contacts = new HashSet<ContactData>();
 
     public GroupData(){
         id = 0;
         name = "";
         header = "";
         footer = "";
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -45,8 +50,8 @@ public class GroupData {
         return footer;
     }
 
-    public int getId() {
-        return id;
+    public Contacts getContacts() {
+        return new Contacts(contacts);
     }
 
     public GroupData withId(int id) {
