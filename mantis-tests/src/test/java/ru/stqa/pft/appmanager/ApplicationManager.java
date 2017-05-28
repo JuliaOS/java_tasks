f2db8e0bbba3dd141775205f9f1dc34094e449d3
JuliaOS <julia2790@mail.ru>
2017-05-28 12:28:50 +0300

    Added: 2 layers structure for mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/appmanager/ApplicationManager.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Julia on 4/16/2017.
+ */
+public class ApplicationManager {
+
+    WebDriver wd;
+    private String browser;
+    private Properties properties;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+    public void init() throws IOException {
+        if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)){
+            wd = new InternetExplorerDriver();
+        }
+        properties= new Properties();
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.BaseUrl"));
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+}
---
+++ mantis-tests/src/test/java/ru/stqa/pft/tests/TestBase.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+/**
+ * Created by Julia on 4/16/2017.
+ */
+public class TestBase {
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite (alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p){
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method m){
+        logger.info("Stop test " + m.getName());
+    }
+}
