7500b9db01fe4d944d0faa76228c963b79bdedf3
JuliaOS <julia2790@mail.ru>
2017-05-28 13:24:24 +0300

    Added: login test

---
+++ mantis-tests/src/test/java/ru/stqa/pft/tests/LoginTests.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.appmanager.HttpSession;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by Julia on 5/28/2017.
+ */
+public class LoginTests extends TestBase {
+
+    @Test
+
+    public void loginTests() throws IOException {
+        HttpSession session = app.newSession();
+        assertTrue(session.login("administrator", "root"));
+        assertTrue(session.isLoggedInAs("administrator"));
+    }
+}
