1aa727663bbd1f0f43bb5a2e8261481abf426f7a
JuliaOS <julia2790@mail.ru>
2017-06-08 20:21:34 +0300

    Modified; sandbox tests

--- Task1/sandbox/src/test/java/t1/PrimeTests.java
+++ Task1/sandbox/src/test/java/t1/PrimeTests.java
@@ -7,10 +7,10 @@ import org.testng.annotations.Test;
  * Created by Julia on 4/30/2017.
  */
 
-@Test
+@Test(enabled=false)
 public class PrimeTests {
 
     public void testPrimeTrue(){
-        Assert.assertTrue(Primes.isPrime(12));
+        Assert.assertTrue(Primes.isPrime(3));
     }
 }
